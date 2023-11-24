c018a5763c4700a92c1b496dbcc5157b52eaea84
SLachmaiyaSAE <136556179+SLachmaiyaSAE@users.noreply.github.com>
2023-11-24 16:27:16 +1100

    Added levels and realigned scene loader

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,35 +10,54 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(4);
-    }
-
-    public void Levels()
-    {
         SceneManager.LoadSceneAsync(1);
-    }
-    public void Options()
-    {
-        SceneManager.LoadSceneAsync(2);
-    }
-    public void Credits()
-    {
-        SceneManager.LoadSceneAsync(3);
     }
 
     public void Level1()
     {
-        SceneManager.LoadSceneAsync(4);
+        SceneManager.LoadSceneAsync(1);
     }
 
     public void Level2()
     {
-        SceneManager.LoadSceneAsync(5);
+        SceneManager.LoadSceneAsync(2);
     }
 
     public void Level3()
     {
+        SceneManager.LoadSceneAsync(3);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadSceneAsync(4);
+    }
+
+    public void Level5()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }
+
+    public void Level6()
+    {
         SceneManager.LoadSceneAsync(6);
+    }
+
+    public void Level7()
+    {
+        SceneManager.LoadSceneAsync(7);
+    }
+        public void Level8()
+    {
+        SceneManager.LoadSceneAsync(8);
+    }
+    public void Level9()
+    {
+        SceneManager.LoadSceneAsync(9);
+    }
+    public void Level10()
+    {
+        SceneManager.LoadSceneAsync(10);
     }
 
 
